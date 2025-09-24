636d350d2cac109f9de94a32652a8f43516f32cc
Kyle Austad <kyleaustad@gmail.com>
2025-09-24 14:08:26 -0500

    logo to the landing

--- src/features/hero/components/hero-text.tsx
+++ src/features/hero/components/hero-text.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { useIsMobile } from "@/hooks/use-mobile";
 
 export const HeroText = () => {
@@ -7,12 +8,32 @@ export const HeroText = () => {
 
   if (isMobile) {
     return (
-      <h1 className="text-4xl font-script text-primary">Letters From Santa</h1>
+      <div className="flex flex-col items-center gap-4 w-full">
+        <Image
+          src="/logo.webp"
+          alt="Logo"
+          width={150}
+          height={150}
+          className="p-0 m-0"
+          priority
+        />
+        <h1 className="text-5xl font-script text-primary">
+          Letters From Santa
+        </h1>
+      </div>
     );
   }
 
   return (
     <div className="flex flex-col items-center gap-4 w-full ">
+      <Image
+        src="/logo.webp"
+        alt="Logo"
+        width={150}
+        height={150}
+        className="p-0 m-0"
+        priority
+      />
       <div className="curved-text-container w-full">
         <svg viewBox="0 0 1000 200" className="w-full h-48 md:h-48 sm:h-32">
           <defs>
@@ -30,14 +51,7 @@ export const HeroText = () => {
         </svg>
         {/* center the logo right below the text in the middle of the curve */}
         {/* <div className="absolute top-38 left-1/2 -translate-x-1/2 -translate-y-1/2">
-            <Image
-              src="/logo.webp"
-              alt="Logo"
-              width={100}
-              height={100}
-              className=""
-              priority
-            />
+           
           </div> */}
       </div>
     </div>
